834928faf0ea44a1cf1e464982aded5907ac8a4a
agahkarakuzu <agahkarakuzu@gmail.com>
2020-06-08 19:17:00 -0400

    [UPLOAD] Test data

--- downloadTestData.sh
+++ downloadTestData.sh
@@ -1,2 +1,4 @@
-dowloadLink = 
-curl -o Test/testData.zip $dowloadLink 2>/dev/null || wget -O Test/testData.zip $dowloadLink
+dowloadLink=https://osf.io/9ptbr/download?version=1
+echo $dowloadLink
+wget -O Test/testData.zip $dowloadLink 2>/dev/null || curl $dowloadLink --output $PWD/Test/testData.zip -O -J -L
+unzip Test/testData.zip -d Test
