3c633666517347553c2501e84250e0324f2cf24c
Agah <agahkarakuzu@gmail.com>
2020-06-08 20:29:35 -0400

    Update downloadTestData.sh

--- downloadTestData.sh
+++ downloadTestData.sh
@@ -1,4 +1,4 @@
 dowloadLink=https://osf.io/9ptbr/download?version=2
 echo $dowloadLink
-wget -O $PWD/Test/testData.zip $dowloadLink 2>/dev/null || curl $dowloadLink --output $PWD/Test/testData.zip -O -J -L
-unzip Test/testData.zip -d Test
+wget -O $GITHUB_WORKSPACE/Test/testData.zip $dowloadLink 2>/dev/null || curl $dowloadLink --output $GITHUB_WORKSPACE/Test/testData.zip -O -J -L
+unzip $GITHUB_WORKSPACE/Test/testData.zip -d Test
